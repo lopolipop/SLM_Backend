bfe7d004251882de24922efea93066b49514614a
BANKK <lopolipop@gmail.com>
2016-09-29 01:53:34 +0700

    เพิ่ม method สำหรับเช็คเวลาสำหรับ update rss feed

--- SLM_Backend/src/slm_backend/SLM_Backend.java
+++ SLM_Backend/src/slm_backend/SLM_Backend.java
@@ -8,6 +8,10 @@ package slm_backend;
 import java.io.UnsupportedEncodingException;
 import static java.lang.System.exit;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,19 +24,37 @@ public class SLM_Backend {
     /**
      */
     public static void main(String[] args) {
-        Database_Controller db_conn = new Database_Controller();
+        while (true) {
+            try {
+                RSS_Timer();
+                Thread.sleep(60000); //Millisecond
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void RSS_Timer() {
         try {
-            while (true) {
+            Database_Controller db_conn = new Database_Controller();
+
+            int alarm_hour = 0;
+            int alarm_minute = 1;
+
+            Calendar cNow = Calendar.getInstance();
+            int now_hour = cNow.get(Calendar.HOUR);
+            int now_minute = cNow.get(Calendar.MINUTE);
+
+            if (alarm_hour == now_hour && alarm_minute == now_minute) {
                 Connection connection = db_conn.Database_Connector();
                 RSS_Feed rss_feed = new RSS_Feed();
                 rss_feed.getFeed(connection, "it");
                 rss_feed.getFeed(connection, "cS");
                 db_conn.Database_close(connection);
-                Thread.sleep(5000); //Millisecond
             }
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            exit(0);
         }
+
     }
 }
